9a291f34a24451f2bf2563622b7e4f86b92aab2f
viniciusomarcos <oliveiramarcosvinicius4@gmail.com>
2025-06-15 19:36:33 -0300

    Commit 6 - Fatura em HTML

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function formatarMoeda(valor) {
   }).format(valor / 100);
 }
 
-
 function getPeca(pecas, apre) {
   return pecas[apre.id];
 }
@@ -16,7 +15,6 @@ function getPeca(pecas, apre) {
 function calcularCredito(pecas, apre) {
   let creditos = 0;
   creditos += Math.max(apre.audiencia - 30, 0);
-  
   if (getPeca(pecas, apre).tipo === "comedia")
     creditos += Math.floor(apre.audiencia / 5);
   return creditos;
@@ -56,21 +54,37 @@ function calcularTotalCreditos(pecas, apresentacoes) {
 
 function gerarFaturaStr(fatura, pecas) {
   let faturaStr = `Fatura ${fatura.cliente}\n`;
-
   for (let apre of fatura.apresentacoes) {
     faturaStr += `  ${getPeca(pecas, apre).nome}: ${formatarMoeda(calcularTotalApresentacao(pecas, apre))} (${apre.audiencia} assentos)\n`;
   }
-
- 
   faturaStr += `Valor total: ${formatarMoeda(calcularTotalFatura(pecas, fatura.apresentacoes))}\n`;
   faturaStr += `Créditos acumulados: ${calcularTotalCreditos(pecas, fatura.apresentacoes)} \n`;
-
   return faturaStr;
+}
+
+// Nova função para gerar a fatura em formato HTML
+function gerarFaturaHTML(fatura, pecas) {
+  let faturaHTML = `<html><p> Fatura ${fatura.cliente} </p>`;
+  faturaHTML += "<ul>";
+  for (let apre of fatura.apresentacoes) {
+    faturaHTML += `<li>  ${getPeca(pecas, apre).nome}: ${formatarMoeda(calcularTotalApresentacao(pecas, apre))} (${apre.audiencia} assentos) </li>`;
+  }
+  faturaHTML += "</ul>";
+  faturaHTML += `<p> Valor total: ${formatarMoeda(calcularTotalFatura(pecas, fatura.apresentacoes))} </p>`;
+  faturaHTML += `<p> Créditos acumulados: ${calcularTotalCreditos(pecas, fatura.apresentacoes)} </p>`;
+  faturaHTML += "</html>";
+  return faturaHTML;
 }
 
 
 // --- Execução do Código ---
 const faturas = JSON.parse(readFileSync('./faturas.json'));
 const pecas = JSON.parse(readFileSync('./pecas.json'));
+
+// Gera e exibe a fatura em texto plano
 const faturaStr = gerarFaturaStr(faturas, pecas);
 console.log(faturaStr);
+
+// Gera e exibe a fatura em HTML
+const faturaHTML = gerarFaturaHTML(faturas, pecas);
+console.log(faturaHTML);
