bf6d3db11494f38cfedef468f85400eb55034796
viniciusomarcos <oliveiramarcosvinicius4@gmail.com>
2025-06-15 16:04:02 -0300

    Commit 3 - Mais Extract Functions

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const { readFileSync } = require('fs');
 function gerarFaturaStr (fatura, pecas) {
     let totalFatura = 0;
     let creditos = 0;
+    
     let faturaStr = `Fatura ${fatura.cliente}\n`;
-    const formato = new Intl.NumberFormat("pt-BR",
+    /*const formato = new Intl.NumberFormat("pt-BR",
                           { style: "currency", currency: "BRL",
-                            minimumFractionDigits: 2 }).format;
+                            minimumFractionDigits: 2 }).format;*/
   
     for (let apre of fatura.apresentacoes) {
       //const peca = pecas[apre.id];
@@ -14,15 +15,14 @@ function gerarFaturaStr (fatura, pecas) {
       let total = calcularTotalApresentacao(apre);
   
       // créditos para próximas contratações
-      creditos += Math.max(apre.audiencia - 30, 0);
-      if (getPeca(apre).tipo === "comedia") 
-         creditos += Math.floor(apre.audiencia / 5);
-  
+      creditos += calcularCredito(apre);
+
       // mais uma linha da fatura
-      faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+      //formato = formatarMoeda(totalFatura);
+      faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(total)} (${apre.audiencia} assentos)\n`;
       totalFatura += total;
     }
-    faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
+    faturaStr += `Valor total: ${formatarMoeda(totalFatura)}\n`;
     faturaStr += `Créditos acumulados: ${creditos} \n`;
     return faturaStr;
   }
@@ -50,9 +50,23 @@ function calcularTotalApresentacao(apre){
 }
 
 // função query
-  function getPeca(apresentacao) {
-    return pecas[apresentacao.id];
-  }
+function getPeca(apresentacao) {
+  return pecas[apresentacao.id];
+}
+
+function calcularCredito(apre) {
+  let creditos = 0;
+  creditos += Math.max(apre.audiencia - 30, 0);
+  if (getPeca(apre).tipo === "comedia") 
+      creditos += Math.floor(apre.audiencia / 5);
+  return creditos;   
+}
+
+function formatarMoeda(valor) {
+  return new Intl.NumberFormat("pt-BR",
+    { style: "currency", currency: "BRL",
+      minimumFractionDigits: 2 }).format(valor/100);
+}
 
 const faturas = JSON.parse(readFileSync('./faturas.json'));
 const pecas = JSON.parse(readFileSync('./pecas.json'));
